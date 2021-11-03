c842a17640c0fa1806f276861d30b21ef6f9a5f4
DilankaM <dilanka85@gmail.com>
2021-11-03 12:41:41 +0530

    inital code commit

---
+++ src/main/java/io/dilankam/protobuffer/ProtoBufferApplication.java
@@ -0,0 +1,13 @@
+package io.dilankam.protobuffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoBufferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtoBufferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dilankam/protobuffer/ProtoBufferApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dilankam.protobuffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoBufferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
